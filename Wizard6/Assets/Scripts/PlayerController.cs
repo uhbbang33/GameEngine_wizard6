da346f3f151bb18d7dda913a68e36354290f13df
JHJ-dev <wjs3662@gmail.com>
2021-05-07 01:28:58 +0900

    Player Update

--- Wizard6/Assets/Scripts/PlayerController.cs
+++ Wizard6/Assets/Scripts/PlayerController.cs
@@ -1,65 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] bool lockCursor = true;
-    [SerializeField] private float walkSpeed;
-    
-    [SerializeField] private float upSensitivity;
-    [SerializeField] private float sideSensitivity;
-    private float cameraRotationLimit = 90.0f;
-    private float currentCameraRotationX = 0;
+    public Camera cam;
+    public Rigidbody rb;
 
-    [SerializeField] private Camera theCamera;
-    private Rigidbody myRigid;
+    public float speed = 10;
 
-    void Start()
+    private void Start()
     {
-        myRigid = GetComponent<Rigidbody>();
-        if (lockCursor)
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
-    }
-    
-    void Update()
-    {
-        PlayerMove();
-        CameraRotate();
-        PlayerRotate();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void PlayerMove()
+    private void Update()
     {
-        float _mouseDirX = Input.GetAxisRaw("Horizontal");
-        float _mouseDirZ = Input.GetAxisRaw("Vertical");
+        //camera rotation
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
 
-        Vector3 _moveHorizontal = transform.right * _mouseDirX;
-        Vector3 _moveVertical = transform.forward * _mouseDirZ;
+        transform.eulerAngles = transform.eulerAngles + new Vector3(0, mouseX, 0);
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        cam.transform.eulerAngles = cam.transform.eulerAngles + new Vector3(-mouseY, 0, 0);
 
-        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
-    }
+        //player movement
+        var keyboardX = Input.GetAxis("Horizontal");
+        var keyboardY = Input.GetAxis("Vertical");
 
-    void CameraRotate()
-    {
-        float _XRotation = Input.GetAxisRaw("Mouse Y");
-        float _cameraRotationX = _XRotation * upSensitivity;
-        currentCameraRotationX -= _cameraRotationX;
-        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
-
-        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
-    }
-
-    void PlayerRotate()
-    {
-        float _YRotation = Input.GetAxisRaw("Mouse X");
-        Vector3 _playerRotationY = new Vector3(0f, _YRotation, 0f) * sideSensitivity;
-        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_playerRotationY));
+        rb.velocity = (transform.forward * (keyboardY * speed)) + 
+                      (transform.right * (keyboardX * speed));
     }
 }
